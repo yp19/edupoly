1fbf39ca035737f8c62cae3ea69c943d580af313
Padmaja <ypadmaja19@gmail.com>
2024-02-14 14:19:32 -0800

    redux fundament

---
+++ nem29/hai.js
@@ -0,0 +1,8 @@
+var os = require('os')
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.freemem());
+console.log(os.constants);
+console.log(os.cpus());
+console.log(os.networkInterfaces());
+console.log(os.version());
